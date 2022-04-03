9a50e6ac2ae4a7c384781b7c398a1d46f4f7f100
Om More <51229945+thisisommore@users.noreply.github.com>
2022-04-03 12:21:52 +0530

    Validate wallet address (#6)
    * Validate wallet address
    
    * Install gotestsum using go install, and do that as first step
    
    * Remove codecov

--- api/v1/flowid/flowid_test.go
+++ api/v1/flowid/flowid_test.go
@@ -20,13 +20,28 @@ func Test_GetFlowId(t *testing.T) {
 	logwrapper.Init("../../../logs")
 	t.Cleanup(testingcommon.DeleteCreatedEntities())
 	gin.SetMode(gin.TestMode)
+	testWalletAddress := testingcommon.GenerateWallet().WalletAddress
+	u, err := url.Parse("/api/v1.0/flowid")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Run("Should fail if wallet address is not hexadecimal", func(t *testing.T) {
+		q := url.Values{}
+		q.Set("walletAddress", "invalidwalletaddr")
+		u.RawQuery = q.Encode()
+		rr := httptest.NewRecorder()
 
-	t.Run("Should be able to get flow id", func(t *testing.T) {
-		testWalletAddress := testingcommon.GenerateWallet().WalletAddress
-		u, err := url.Parse("/api/v1.0/flowid")
+		req, err := http.NewRequest("GET", u.String(), nil)
 		if err != nil {
-			t.Fatal(err)
+			t.Error(err)
 		}
+		c, _ := gin.CreateTestContext(rr)
+		c.Request = req
+		GetFlowId(c)
+		assert.Equal(t, rr.Result().StatusCode, http.StatusBadRequest)
+	})
+	t.Run("Should be able to get flow id", func(t *testing.T) {
+
 		q := url.Values{}
 		q.Set("walletAddress", testWalletAddress)
 		u.RawQuery = q.Encode()
