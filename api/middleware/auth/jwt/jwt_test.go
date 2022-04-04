57f3dd5dd08be7afab58e832c0bdb2277c553000
Om More <51229945+thisisommore@users.noreply.github.com>
2022-04-04 22:57:23 +0530

    jwt: test token expiration (#9)

--- api/middleware/auth/jwt/jwt_test.go
+++ api/middleware/auth/jwt/jwt_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/TheLazarusNetwork/marketplace-engine/config"
 	"github.com/TheLazarusNetwork/marketplace-engine/config/dbconfig"
@@ -14,6 +15,7 @@ import (
 	"github.com/TheLazarusNetwork/marketplace-engine/util/pkg/envutil"
 	"github.com/TheLazarusNetwork/marketplace-engine/util/pkg/logwrapper"
 	"github.com/TheLazarusNetwork/marketplace-engine/util/testingcommon"
+	jwt "github.com/golang-jwt/jwt/v4"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
@@ -52,6 +54,26 @@ func Test_JWT(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
+		statusCode := callApi(t, token)
+		assert.Equal(t, http.StatusForbidden, statusCode)
+	})
+
+	t.Run("Should return 403 with expired JWT", func(t *testing.T) {
+		expiration := time.Now().Add(time.Second * 2)
+		signedBy := envutil.MustGetEnv("SIGNED_BY")
+		newClaims := claims.CustomClaims{
+			WalletAddress: testWalletAddress,
+			SignedBy:      signedBy,
+			RegisteredClaims: jwt.RegisteredClaims{
+				ExpiresAt: jwt.NewNumericDate(expiration),
+			},
+		}
+		time.Sleep(time.Second * 2)
+		token, err := auth.GenerateToken(newClaims, envutil.MustGetEnv("JWT_PRIVATE_KEY"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
 		statusCode := callApi(t, token)
 		assert.Equal(t, http.StatusForbidden, statusCode)
 	})
