4ef2852b098fefd4a02e9173dfb9040f5dcfcadf
Om More <51229945+thisisommore@users.noreply.github.com>
2022-04-13 20:05:59 +0530

    Introduce APP_PORT env variable (#16)

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/TheLazarusNetwork/marketplace-engine/app"
+	"github.com/TheLazarusNetwork/marketplace-engine/util/pkg/envutil"
 	"github.com/TheLazarusNetwork/marketplace-engine/util/pkg/logwrapper"
 )
 
 func main() {
 	app.Init(".env", "logs")
 	logwrapper.Log.Info("Starting app")
-	err := app.GinApp.Run(":8000")
+	err := app.GinApp.Run(":" + envutil.MustGetEnv("APP_PORT"))
 	if err != nil {
 		logwrapper.Log.Fatal(err)
 	}
