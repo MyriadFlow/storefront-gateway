c20aa7a89611d19b37cd82204f5355f307655f58
thisisommore <ommore501@gmail.com>
2022-06-16 17:45:32 +0000

    Fix details test

--- api/v1/details/details_test.go
+++ api/v1/details/details_test.go
@@ -21,12 +21,38 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_PostDetails(t *testing.T) {
+func Test_Details(t *testing.T) {
 	config.Init("../../../.env")
 	logwrapper.Init("../../../logs")
 	dbinit.Init()
 	t.Cleanup(testingcommon.DeleteCreatedEntities())
 	gin.SetMode(gin.TestMode)
+
+	t.Run("Should be able to get org details", func(t *testing.T) {
+		_ = "/api/v1.0/details"
+		rr := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(rr)
+		getDetails(c)
+		var response types.ApiResponse
+		body := rr.Body
+		json.NewDecoder(body).Decode(&response)
+		var org Org.Org
+		testingcommon.ExtractPayload(&response, &org)
+		assert.Equal(t, http.StatusOK, rr.Result().StatusCode)
+		assert.Equal(t, envutil.MustGetEnv("ORG_NAME"), org.Name)
+		assert.Equal(t, envutil.MustGetEnv("HOME_TITLE"), org.HomeTitle)
+		assert.Equal(t, envutil.MustGetEnv("HOME_DESCRIPTION"), org.HomeDescription)
+		assert.Equal(t, envutil.MustGetEnv("GRAPH_URL"), org.GraphUrl)
+		assert.Equal(t, envutil.MustGetEnv("CREATIFY_CONTRACT_ADDRESS"), org.CreatifyAddress)
+		assert.Equal(t, envutil.MustGetEnv("MARKETPLACE_CONTRACT_ADDRESS"), org.MarketPlaceAddress)
+		assert.Equal(t, envutil.MustGetEnv("FOOTER"), org.Footer)
+
+		assert.Equal(t, envutil.MustGetEnv("TOP_HIGHLIGHTS"), strings.Join(org.TopHighlights, ","))
+		assert.Equal(t, envutil.MustGetEnv("TRENDINGS"), strings.Join(org.Trendings, ","))
+		assert.Equal(t, envutil.MustGetEnv("TOP_BIDS"), strings.Join(org.TopBids, ","))
+		logrus.Debug(org)
+	})
+
 	t.Run("Should be able to update org details", func(t *testing.T) {
 		url := "/api/v1.0/details"
 		rr := httptest.NewRecorder()
@@ -48,41 +74,23 @@ func Test_PostDetails(t *testing.T) {
 		c.Request = req
 		postDetails(c)
 		assert.Equal(t, http.StatusOK, rr.Result().StatusCode)
-	})
-}
 
-func Test_GetDetails(t *testing.T) {
-	config.Init("../../../.env")
-	logwrapper.Init("../../../logs")
-	dbinit.Init()
-	t.Cleanup(testingcommon.DeleteCreatedEntities())
-	gin.SetMode(gin.TestMode)
-	t.Run("Should be able to get org details", func(t *testing.T) {
-		_ = "/api/v1.0/details"
-		rr := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(rr)
-		getDetails(c)
-		var response types.ApiResponse
-		body := rr.Body
-		json.NewDecoder(body).Decode(&response)
-		var org Org.Org
-		testingcommon.ExtractPayload(&response, &org)
-		assert.Equal(t, http.StatusOK, rr.Result().StatusCode)
-		assert.Equal(t, "Max", org.Name)
-		assert.Equal(t, envutil.MustGetEnv("HOME_TITLE"), org.HomeTitle)
-		assert.Equal(t, envutil.MustGetEnv("HOME_DESCRIPTION"), org.HomeDescription)
-		assert.Equal(t, envutil.MustGetEnv("GRAPH_URL"), org.GraphUrl)
-		assert.Equal(t, envutil.MustGetEnv("CREATIFY_CONTRACT_ADDRESS"), org.CreatifyAddress)
-		assert.Equal(t, envutil.MustGetEnv("MARKETPLACE_CONTRACT_ADDRESS"), org.MarketPlaceAddress)
-		assert.Equal(t, envutil.MustGetEnv("FOOTER"), org.Footer)
-
-		assert.Equal(t,
-			"43", strings.Join(org.TopHighlights, ","),
-		)
-		assert.Equal(t, "47", strings.Join(org.Trendings, ","))
-		assert.Equal(t, "42", strings.Join(org.TopBids, ","))
-
-		logrus.Debug(org)
+		t.Cleanup(func() {
+			err = Org.UpdateOrg(
+				Org.Org{
+					Name:               envutil.MustGetEnv("ORG_NAME"),
+					HomeTitle:          envutil.MustGetEnv("HOME_TITLE"),
+					HomeDescription:    envutil.MustGetEnv("HOME_DESCRIPTION"),
+					GraphUrl:           envutil.MustGetEnv("GRAPH_URL"),
+					CreatifyAddress:    envutil.MustGetEnv("CREATIFY_CONTRACT_ADDRESS"),
+					MarketPlaceAddress: envutil.MustGetEnv("MARKETPLACE_CONTRACT_ADDRESS"),
+					Footer:             envutil.MustGetEnv("FOOTER"),
+					TopHighlights:      strings.Split(envutil.MustGetEnv("TOP_HIGHLIGHTS"), ","),
+					Trendings:          strings.Split(envutil.MustGetEnv("TRENDINGS"), ","),
+					TopBids:            strings.Split(envutil.MustGetEnv("TOP_BIDS"), ","),
+				},
+			)
+		})
 	})
 
 }
