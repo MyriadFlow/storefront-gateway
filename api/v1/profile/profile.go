8096bcce1feb4528dbc879f397492c5a83e4f5bf
Sonu2k23 <sonu2k23@protonmail.com>
2023-02-04 20:37:53 +0530

    resolve patch update and more user info added

--- api/v1/profile/profile.go
+++ api/v1/profile/profile.go
@@ -24,16 +24,15 @@ func ApplyRoutes(r *gin.RouterGroup) {
 
 func patchProfile(c *gin.Context) {
 	db := dbconfig.GetDb()
-	var requestBody PatchProfileRequest
-	err := c.BindJSON(&requestBody)
+	var user models.User
+	err := c.BindJSON(&user)
 	if err != nil {
 		httphelper.ErrResponse(c, http.StatusForbidden, "payload is invalid")
 		return
 	}
 	walletAddress := c.GetString("walletAddress")
-	result := db.Model(&models.User{}).
-		Where("wallet_address = ?", walletAddress).
-		Updates(requestBody)
+	result := db.Model(&models.User{}).Where("wallet_address = ?", walletAddress).Updates(user)
+
 	if result.Error != nil {
 		httphelper.ErrResponse(c, http.StatusInternalServerError, "Unexpected error occured")
 
@@ -52,7 +51,8 @@ func getProfile(c *gin.Context) {
 	db := dbconfig.GetDb()
 	walletAddress := c.GetString("walletAddress")
 	var user models.User
-	err := db.Model(&models.User{}).Select("name, profile_picture_url,country, wallet_address").Where("wallet_address = ?", walletAddress).First(&user).Error
+	//err := db.Model(&models.User{}).Select("name, profile_picture_url,country, wallet_address").Where("wallet_address = ?", walletAddress).First(&user).Error
+	err := db.Model(&models.User{}).Where("wallet_address = ?", walletAddress).First(&user).Error
 	if err != nil {
 		logrus.Error(err)
 		httphelper.ErrResponse(c, http.StatusInternalServerError, "Unexpected error occured")
@@ -61,7 +61,7 @@ func getProfile(c *gin.Context) {
 	}
 
 	payload := GetProfilePayload{
-		user.Name, user.WalletAddress, user.ProfilePictureUrl, user.Country,
+		user.Name, user.WalletAddress, user.ProfilePictureUrl, user.Country,user.FacebookId, user.InstagramId,
 	}
 	httphelper.SuccessResponse(c, "Profile fetched successfully", payload)
 }
--- models/User.go
+++ models/User.go
@@ -6,4 +6,6 @@ type User struct {
 	FlowIds           []FlowId `gorm:"foreignkey:WalletAddress" json:"-"`
 	ProfilePictureUrl string   `json:"profilePictureUrl,omitempty"`
 	Country           string   `json:"country,omitempty"`
+	FacebookId           string   `json:"facebook_id,omitempty"`
+	InstagramId           string   `json:"instagram_id,omitempty"`
 }
